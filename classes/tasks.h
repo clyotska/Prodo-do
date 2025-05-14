5592eaa90143ec65226ab7dab51d8635a8f8d3a2
Alina Gaybdjanova <cyberiaclair@gmail.com>
2025-05-15 01:01:06 +0500

    Comments and Input Validations
    Yay, I finally added comments and changed the structure of description input validation + added input validation for main menu :3
    
    Yayyayayay, finally I can go to sleep

--- classes/tasks.h
+++ classes/tasks.h
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <string>
+// limits used for cin.ignore() only
 #include <limits>
+// used for some parameters
 #include <optional>
 #include <fstream>
+// used for date created
 #include <ctime>
 
 using namespace std;
@@ -21,6 +24,7 @@ class Task
 {
 
 protected:
+    // all the protected members
     string header;
     optional<string> body;
     time_t dateCreated;
@@ -29,15 +33,21 @@ protected:
     int priority;
 
 public:
+    // Constructor, which we don't even use :)
     Task(string h = "Unknown", optional<string> b = nullopt, time_t dc = 0,
         optional<string> dd = nullopt, bool c = false, int p = 1) : header(h), body(b), dateCreated(dc), dueDate(dd), complete(c), priority(p) {};
 
     void setData()
     {
+        // gigantic ahh function of setting all the data
+        // btw this due date was written by Ilya
+        // Alina (me) wrote all the comments!!
         string temp;
         cout << "Enter the task: ";
         getline(cin, header);
 
+        // proper initialization of dateCreated using ctime
+        // basically just takes date from your device
         time(&dateCreated);
         time_t t = time(nullptr);
         tm *lT = localtime(&t);
@@ -50,33 +60,36 @@ public:
             getline(cin, temp);
             if (cin.good())
             {
-                if (!temp.empty())
+                if (!temp.empty()) // if user decided to add due date
                 {
-                    if (temp.length() == 10)
+                    if (temp.length() == 10) // if format of the date is correct
                     {
-                        string day = temp.substr(0, 2);
-                        string month = temp.substr(3, 2);
-                        string year = temp.substr(6, 4);
+                        string day = temp.substr(0, 2);   // here ilya finds day
+                        string month = temp.substr(3, 2); // here ilya finds month
+                        string year = temp.substr(6, 4);  // here ilya finds year
+
+                        // below Ilya checks whether due date is correct
+                        // basically the range of day, month and year and that due date is not in the past
                         if (temp[2] == '.' && temp[5] == '.' && stoi(day) >= 1 && stoi(day) <= 31 && stoi(month) >= 1 && stoi(month) <= 12 && stoi(year) >= lT->tm_year + 1900 && stoi(month) >= lT->tm_mon && stoi(day) >= lT->tm_mday)
                         {
                             input_success1 = true;
                             dueDate = temp;
                         }
-                        else
+                        else // if user is a little deranged (little dummy dumbo) we try again :3
                         {
                             cout << "Invalid input. Please enter the actual date" << endl;
                             cin.clear();
                         }
                     }
-                    else
+                    else // if user is wrong with format :(
                     {
                         cout << "Invalid input. Please enter the actual date" << endl;
                         cin.clear();
                     }
                 }
-                else
+                else // if user decided not to enter due Date
                 {
-                    dueDate = nullopt;
+                    dueDate = nullopt; // nullopt if from optional library, shows the compiler that nothing is here
                     input_success1 = true;
                 }
             }
@@ -85,63 +98,63 @@ public:
         // checking the choice of description
         int description_choice;
         bool input_success2 = false;
-        while (!input_success2)
+        cout << "Enter 1 to add description and 0 to skip this step" << endl;
+        while (!(cin >> description_choice) || description_choice > 1 || description_choice < 0) // we check the range and whether input is in good condition
         {
-            cout << "Do you want to add additional description?" << endl
-                << "1 for yes, 0 for no: ";
-            cin >> description_choice;
-            if (cin.good())
-            { // Check if the input operation was successful
-                if (description_choice == 0 || description_choice == 1)
-                {
-                    input_success2 = true;
-                    switch (description_choice)
-                    {
-                    case 1:
-                    {
-                        cin.ignore();
-                        cout << "Enter description: " << endl;
-                        string tempBody;
-                        getline(cin, tempBody);
-                        body = tempBody;
-                        break;
-                    }
-                    case 0:
-                        cout << "Great! We continue with no optional description then!" << endl;
-                        cin.ignore();
-                        break;
-                    default:
-                        cerr << "Error: There will be dragons" << endl;
-                    }
-                }
+            // Check if the failure was due to non-integer input
+            if (cin.fail())
+            {
+                cout << "WRONG INPUT. Please enter a number (1 or 0): ";
+                cin.clear();                                         // Clear the error flags (e.g., failbit)
+                cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Discard the invalid input
             }
             else
             {
-                cout << "Invalid input. Please EITHER enter 1 for yes or 0 for no" << endl;
-                cin.clear(); // Clear the error flags (like failbit)
-                // Discard the invalid input from the buffer
+                // Input was an integer, but outside the range 1-7
+                cout << "WRONG NUMBER. Please enter a number (1 or 0): ";
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Discard the rest of the line
             }
+        }
+        switch (description_choice) // switch for adding/not adding description
+        {
+        case 1: // user decided to enter description
+        {       // braces are here, bc it turns out I can't create a variable in not a new scope
+            cin.ignore();
+            cout << "Enter description: " << endl;
+            string tempBody;
+            getline(cin, tempBody);
+            body = tempBody; // now the entered by user description is the task's description
+            break;
+        }
+        case 0:
+            cout << "Great! We continue with no optional description then!" << endl;
+            cin.ignore();
+            break;
+        default:
+            // Something went wrong and we don't know why
+            cerr << "Error: There will be dragons" << endl;
         }
 
         // checking priority
         bool input_success3 = false;
-        while (!input_success3)
+        while (!input_success3) // here I (Alina) used a different approach to check the input
         {
             cout << "Enter priority for your task from 1 to 4: ";
             cin >> priority;
-            if (cin.good())
+            if (cin.good()) // checking whether its integer
             {
-                if (priority >= 1 && priority <= 4)
+                if (priority >= 1 && priority <= 4) // checking whether integer in range
                 {
                     input_success3 = true;
                 }
-                else
+                else // wrong range
                 {
                     cout << "Invalid input. Please enter a number from 1 to 4 ONLY" << endl;
                     cin.ignore(numeric_limits<streamsize>::max(), '\n');
                 }
             }
-            else
+            else // wrong input all together
             {
                 cout << "Invalid input. Please enter a number from 1 to 4 ONLY" << endl;
                 cin.clear();
@@ -150,20 +163,26 @@ public:
         }
     }
 
-    void markAsComplete(){
+    void markAsComplete()
+    { // we mark a task as complete
         complete = true;
     }
 
-    bool getCompletionStatus(){
+    bool getCompletionStatus() const
+    { // const to show that this code doesn't change task parameters
+        // getter for completetion status
         return complete;
     }
 
-    void Display() const
-    {   
-        if (complete){
+    void Display() const // const to show that this code doesn't change task parameters
+    {
+        // display function
+        if (complete)
+        {
             cout << "[+] ";
         }
-        else{
+        else
+        {
             cout << "[-] ";
         }
         cout << "Task: " << header << endl;
@@ -176,24 +195,25 @@ public:
         {
             cout << body.value() << endl;
         }
-        cout << "Priority: " << priority << endl << "-----------------" << endl;
+        cout << "Priority: " << priority << endl
+            << "-----------------" << endl;
     }
 
-    string getHeader() const
+    string getHeader() const // another getter
     {
         return header;
     }
 
-    time_t getDate() const
+    time_t getDate() const // another getter
     {
         return dateCreated;
     }
-    int getPrior() const
+    int getPrior() const // another getter
     {
         return priority;
     }
 
-    optional<string> getBody() const
+    optional<string> getBody() const // another getter
     {
         if (body.has_value())
         {
@@ -202,18 +222,20 @@ public:
         return nullopt;
     }
 
-    void serialize(ofstream &ofs) const
+    void serialize(ofstream &ofs) const // function to properly write all the sizes (bc we have strings, and their size can vary)
     {
-        if (!ofs.is_open() || ofs.fail())
+        if (!ofs.is_open() || ofs.fail()) // we check whether the file is open and everything is oki
         {
             cerr << "Output file is bad" << endl;
             return;
         }
 
+        // we write the size of header to a file
         size_t header_len = header.size();
         ofs.write((char *)&header_len, sizeof(header_len));
         ofs.write(header.c_str(), header_len);
 
+        // we check whether description has value and then write its size to a file
         bool bodyHasValue = body.has_value();
         ofs.write((char *)&bodyHasValue, sizeof(bodyHasValue));
         if (bodyHasValue)
@@ -223,8 +245,10 @@ public:
             ofs.write(body.value().c_str(), body_len);
         }
 
+        // we write the size of dateCreated to a file
         ofs.write((char *)&dateCreated, sizeof(dateCreated));
 
+        // we check whether dueDate has value then write its size to a file
         bool dueDateHasValue = dueDate.has_value();
         ofs.write((char *)&dueDateHasValue, sizeof(dueDateHasValue));
         if (dueDateHasValue)
@@ -234,19 +258,22 @@ public:
             ofs.write(dueDate.value().c_str(), dueDate_len);
         }
 
-        ofs.write((char*)&complete, sizeof(complete));
+        // we write the size of complete to a file
+        ofs.write((char *)&complete, sizeof(complete));
 
+        // we write the size of priority to a file
         ofs.write((char *)&priority, sizeof(priority));
 
-        if (ofs.fail())
+        if (ofs.fail()) // smth went wrong with serialization
         {
             cerr << "Something went wrong with serialization" << endl;
         }
     }
 
-    bool deserialize(ifstream &ifs)
+    bool deserialize(ifstream &ifs) // a function to read a task and its members properly
+    // after each reading of every parameter we check whether everything is oki
     {
-        if (!ifs.is_open() || ifs.fail())
+        if (!ifs.is_open() || ifs.fail()) // we check whether the file for reading is open and everything is good
         {
             cerr << "Input file is bad" << endl;
             return false; // Indicate failure
@@ -255,8 +282,10 @@ public:
         bool oki = true;
 
         size_t header_len = 0;
+        // we read the size of header
         if (ifs.read((char *)&header_len, sizeof(header_len)))
         {
+            // we resize the length of header to fit the header we read from file
             header.resize(header_len);
             if (!ifs.read(&header[0], header_len))
             {
@@ -274,6 +303,7 @@ public:
             return false;
 
         bool body_has_value = false;
+        // we read the size of description if its has value
         if (ifs.read((char *)&body_has_value, sizeof(body_has_value)))
         {
             if (body_has_value)
@@ -281,6 +311,7 @@ public:
                 size_t body_len = 0;
                 if (ifs.read((char *)&body_len, sizeof(body_len)))
                 {
+                    //
                     string temp_body;
                     temp_body.resize(body_len);
                     if (ifs.read(&temp_body[0], body_len))
@@ -322,6 +353,7 @@ public:
         if (!oki)
             return false;
 
+        // we read the size of due Date if its has value
         bool dueDate_has_value = false;
         if (ifs.read((char *)&dueDate_has_value, sizeof(dueDate_has_value)))
         {
@@ -359,6 +391,7 @@ public:
             oki = false;
         }
 
+        // we read the size of completion status
         if (!ifs.read((char *)&complete, sizeof(complete)))
         {
             cerr << "Couldn't read completion status" << endl;
@@ -368,6 +401,7 @@ public:
         if (!oki)
             return false;
 
+        // we read the size of priority
         if (!ifs.read((char *)&priority, sizeof(priority)))
         {
             cerr << "Couldn't read priority" << endl;
--- prodo-do.cpp
+++ prodo-do.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
+// used for cin.ignore()
 #include <limits>
+// used for some parameters
 #include <optional>
 #include "classes/tasks.h"
 #include <fstream>
@@ -17,20 +19,18 @@ Amir Madjidov (U2410125)
 Jahongir Davronov (U2410066)
 */
 
-bool checkingInput1and0(int choice, int correct[])
-{
-    return true;
-}
-
+// first version of empty checker for a file
 bool is_empty(std::ifstream &pFile)
 {
     return pFile.peek() == std::ifstream::traits_type::eof();
 }
 
+// function to add a whole new task to a file
 void writeFile()
 {
     Task t;
     ofstream fout("files/tasks.dat", ios::binary | ios::app);
+    // error checker
     if (!fout.is_open())
     {
         cerr << "Error: Couldn't open 'tasks.dat' for writing" << endl;
@@ -41,105 +41,136 @@ void writeFile()
     fout.close();
 }
 
+// function to read from file
 void readFile()
 {
     Task t;
     ifstream ifs("files/tasks.dat", ios::binary);
-    while (ifs.peek() != EOF)
+    while (ifs.peek() != EOF) // reading till we reach the end of file
     {
         t.deserialize(ifs);
         t.Display();
     }
 }
 
-void adding()
+void adding() // i (Alina) wanted to add an ability to create a task in specific place, but later we gave up on this idea
 {
     writeFile();
 }
 
-bool isEmpty()
+bool isEmpty() // another version of empty checker, now we don't need parameters
+// file to check is already specified in the function
 {
     ifstream ifs("files/tasks.dat", ios::binary);
     return ifs.peek() == EOF;
 }
 
-bool readTask(ifstream& ifs, Task& task) {
-    if (ifs.peek() == EOF) {
+// a function to read one specific task from the file
+bool readTask(ifstream &ifs, Task &task)
+{
+    if (ifs.peek() == EOF)
+    {
         return false;
     }
     return task.deserialize(ifs);
 }
 
-void writeTask(ofstream& ofs, const Task& task) {
+// a function to write one specific task to the file
+void writeTask(ofstream &ofs, const Task &task)
+{
     task.serialize(ofs);
 }
 
-
-void markTaskAsComplete(){
-    string taskHeader;
+// a function to mark as complete
+void markTaskAsComplete()
+{
+    string taskHeader; // we will find a task by header
     cout << "Enter the header of the task you want to mark as complete: ";
 
     getline(cin, taskHeader);
 
+    // here I (Alina) open files to write and read to and from
     ifstream fin("files/tasks.dat", ios::binary);
     ofstream fout_temp("files/temp.dat", ios::binary);
 
-    if (!fin.is_open()) {
+    // checking whether they both are open
+    if (!fin.is_open())
+    {
         cerr << "Couldn't open 'files/tasks.dat' to read" << endl;
         return;
     }
-    if (!fout_temp.is_open()) {
+    if (!fout_temp.is_open())
+    {
         cerr << "Couldn't open 'files/temp.dat' to write" << endl;
         fin.close();
         return;
     }
 
+    // a flag to check whether task is found
     bool here = false;
     Task t;
 
     // Read tasks one by one from the original file
-    while (readTask(fin, t)) {
-        if (t.getHeader() == taskHeader) {
+    while (readTask(fin, t))
+    {
+        if (t.getHeader() == taskHeader)
+        { // if the task was found we mark it as complete
             t.markAsComplete();
-            here = true;
+            here = true; // and flag is changed too
             writeTask(fout_temp, t);
-        } else {
+        }
+        else
+        { // if it's not the task we need, we still write it to a file, just without changing the parameter
             writeTask(fout_temp, t);
         }
     }
 
+    // saving and closing the files
     fin.close();
     fout_temp.close();
 
-    if (!here) {
+    if (!here)
+    { // if no such task was found
         cout << "No such task was found" << endl;
+        // we delete temp too
         remove("files/temp.dat");
-    } else {
-        // Replace the original file with the temporary file
-        if (remove("files/tasks.dat") != 0) {
+    }
+    else
+    {
+        // replace the original file with the temporary file and consequently check the removing operation
+        if (remove("files/tasks.dat") != 0)
+        {
             cerr << "Couldn't delete original file" << endl;
         }
-        if (rename("files/temp.dat", "files/tasks.dat") != 0) {
+        // rename the file and check whether the operation was successful
+        if (rename("files/temp.dat", "files/tasks.dat") != 0)
+        {
             cerr << "Couldn't rename temporary file" << endl;
         }
         cout << "Changes saved to file" << endl;
     }
 }
 
-void sortingComplete(){
+// a function to implement sorting of tasks by completion
+void sortingComplete()
+{
 
-  // Use ios::trunc to ensure temporary files are empty at the start
+    // Use ios::trunc to ensure temporary files are empty at the start
     ofstream foutC("files/taskComplete.dat", ios::binary | ios::trunc);
     ofstream foutI("files/tasksIncomplete.dat", ios::binary | ios::trunc);
     ifstream fin("files/tasks.dat", ios::binary);
 
     // Check if all necessary files opened successfully
-    if (!fin.is_open() || !foutC.is_open() || !foutI.is_open()) {
+    if (!fin.is_open() || !foutC.is_open() || !foutI.is_open())
+    {
         cerr << "Error: Couldn't open necessary files for sorting." << endl;
         // Clean up any files that might have been opened
-        if (fin.is_open()) fin.close();
-        if (foutC.is_open()) foutC.close();
-        if (foutI.is_open()) foutI.close();
+        if (fin.is_open())
+            fin.close();
+        if (foutC.is_open())
+            foutC.close();
+        if (foutI.is_open())
+            foutI.close();
         // Remove temporary files in case of error
         remove("files/taskComplete.dat");
         remove("files/tasksIncomplete.dat");
@@ -148,18 +179,22 @@ void sortingComplete(){
 
     Task t;
     // Read from the main file and write to the appropriate temporary file
-    while(readTask(fin, t))
+    while (readTask(fin, t))
     {
-        if (t.getCompletionStatus() == true){
+        if (t.getCompletionStatus() == true)
+        {
             writeTask(foutC, t);
-             if (foutC.fail()) { // Check for write errors
+            if (foutC.fail())
+            { // Check for write errors
                 cerr << "Error writing to taskComplete.dat during sorting." << endl;
                 break; // Exit loop on error
             }
         }
-        else{
+        else
+        {
             writeTask(foutI, t);
-             if (foutI.fail()) { // Check for write errors
+            if (foutI.fail())
+            { // Check for write errors
                 cerr << "Error writing to tasksIncomplete.dat during sorting." << endl;
                 break; // Exit loop on error
             }
@@ -167,7 +202,8 @@ void sortingComplete(){
     }
 
     // Check if the read loop finished due to an error other than EOF
-    if (fin.fail() && !fin.eof()) {
+    if (fin.fail() && !fin.eof())
+    {
         cerr << "Error reading from tasks.dat during sorting." << endl;
     }
 
@@ -181,12 +217,16 @@ void sortingComplete(){
     ifstream finC("files/taskComplete.dat", ios::binary);
 
     // Check if files for merging opened successfully
-    if (!foutSorted.is_open() || !finI.is_open() || !finC.is_open()) {
+    if (!foutSorted.is_open() || !finI.is_open() || !finC.is_open())
+    {
         cerr << "Error: Couldn't open files for merging during sorting." << endl;
-         // Clean up files
-        if (foutSorted.is_open()) foutSorted.close();
-        if (finI.is_open()) finI.close();
-        if (finC.is_open()) finC.close();
+        // Clean up files
+        if (foutSorted.is_open())
+            foutSorted.close();
+        if (finI.is_open())
+            finI.close();
+        if (finC.is_open())
+            finC.close();
         // Remove temporary files
         remove("files/taskComplete.dat");
         remove("files/tasksIncomplete.dat");
@@ -196,30 +236,34 @@ void sortingComplete(){
 
     // Copy incomplete tasks first
     Task tempTask;
-    while(readTask(finI, tempTask)) {
+    while (readTask(finI, tempTask))
+    {
         writeTask(foutSorted, tempTask);
-         if (foutSorted.fail()) { // Check for write errors
+        if (foutSorted.fail())
+        { // Check for write errors
             cerr << "Error writing incomplete task to tasksSorted.dat." << endl;
             break; // Exit loop on error
         }
     }
-    if (finI.fail() && !finI.eof()) {
+    if (finI.fail() && !finI.eof())
+    {
         cerr << "Error reading from tasksIncomplete.dat during merge." << endl;
     }
 
-
     // Then copy complete tasks
-    while(readTask(finC, tempTask)) {
+    while (readTask(finC, tempTask))
+    {
         writeTask(foutSorted, tempTask);
-         if (foutSorted.fail()) { // Check for write errors
+        if (foutSorted.fail())
+        { // Check for write errors
             cerr << "Error writing complete task to tasksSorted.dat." << endl;
             break; // Exit loop on error
         }
     }
-    if (finC.fail() && !finC.eof()) {
+    if (finC.fail() && !finC.eof())
+    {
         cerr << "Error reading from taskComplete.dat during merge." << endl;
     }
-
 
     finI.close();
     finC.close();
@@ -229,23 +273,28 @@ void sortingComplete(){
     remove("files/taskComplete.dat");
     remove("files/tasksIncomplete.dat");
 
-
     // Replace the original file with the sorted file
-    if (remove("files/tasks.dat") != 0) {
+    if (remove("files/tasks.dat") != 0)
+    {
         cerr << "Error deleting original file during sorting." << endl;
-         // Handle error: The original file might still exist, and the sorted temp file too.
+        // Handle error: The original file might still exist, and the sorted temp file too.
     }
-    if (rename("files/tasksSorted.dat", "files/tasks.dat") != 0) {
+    if (rename("files/tasksSorted.dat", "files/tasks.dat") != 0)
+    {
         cerr << "Error renaming sorted temporary file." << endl;
-         // Handle error: The sorted temp file might still exist.
+        // Handle error: The sorted temp file might still exist.
     }
     cout << "Tasks sorted by completion status." << endl;
 }
 
+/*
+Ilya zaraza dobav' komenty k svoemu kodu sam. Ya ustala i ne obyazana. !!!!!
+*/
 void deleting() {}
 
 void editing() {}
 
+// a display function, where I (Alina) included sorting and used readFile
 void display()
 {
     cout << "---------- All your tasks: ----------" << endl;
@@ -261,13 +310,13 @@ int main()
     {
         cout << "----------------------" << endl;
         // change it to display by default and asking to create a new one if no is there
-        if (isEmpty())
+        if (isEmpty()) // if file is empty we jump to creation
         {
             cout << "Let's create your FIRST task!" << endl;
             adding();
             display();
         }
-        else
+        else // if file is not empty, program displays the tasks
         {
             display();
         }
@@ -278,16 +327,32 @@ int main()
         cout << "4. Delete Task" << endl;
         cout << "5. Display Tasks" << endl;
         cout << "To QUIT enter 6: ";
-        cin >> mainMenuChoice;
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        cout << "----------------------" << endl;
 
-        // add sorting from complete to incomplete
+        while (!(cin >> mainMenuChoice) || mainMenuChoice < 1 || mainMenuChoice > 6) // we check the range and whether input is in good condition
+        {
+            // Check if the failure was due to non-integer input
+            if (cin.fail())
+            {
+                cout << "WRONG INPUT. Please enter a number (from 1 to 6): ";
+                cin.clear();                                         // Clear the error flags (e.g., failbit)
+                cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Discard the invalid input
+            }
+            else
+            {
+                // Input was an integer, but outside the range 1-7
+                cout << "WRONG NUMBER. Please enter a number (from 1 to 6): ";
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Discard the rest of the line
+            }
+        }
+
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
         // add checker for main menu
         // add editing and deleting
         // add smth to inherit
-        
-        switch (mainMenuChoice)
+
+        switch (mainMenuChoice) // a switch for menu options
         {
         case 1:
             adding();
